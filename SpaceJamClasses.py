f61fc23f95623768467deafe7217cd59f99da5d5
joobata <jacobbranum04@gmail.com>
2025-04-02 23:07:11 -0400

    Update SpaceJamClasses.py

--- SpaceJamClasses.py
+++ SpaceJamClasses.py
@@ -1,6 +1,8 @@
 from direct.showbase.ShowBase import ShowBase
 from panda3d.core import *
 from direct.task import Task
+from direct.task.Task import TaskManager
+import DefensePaths as defensePaths
 from CollideObjectBase import *
 
 global base
@@ -69,3 +71,43 @@ class PowerUp(SphereCollidableObject):
         self.modelNode.setName(nodeName)
         tex = loader.loadTexture(texPath)
         self.modelNode.setTexture(tex, 1)
+
+class Orbiter(SphereCollidableObject):
+    numOrbits = 0
+    velocity = 0.005
+    cloudTimer = 240
+    def __init__(self, loader: Loader, taskMgr: TaskManager, modelPath: str, parentNode: NodePath, nodeName: str, scaleVec: Vec3, texPath: str,
+        centralObject: PlacedObject, orbitRadius: float, orbitType: str, staringAt: Vec3):
+        super(Orbiter, self).__init__(loader, modelPath, parentNode, nodeName, Vec3(0, 0, 0), 1.05)
+        self.taskMgr = taskMgr
+        self.orbitType = orbitType
+        self.modelNode.setScale(scaleVec)
+        tex = loader.loadTexture(texPath)
+        self.modelNode.setTexture(tex, 1)
+        self.orbitObject = centralObject
+        self.orbitRadius = orbitRadius
+        self.staringAt = staringAt
+        Orbiter.numOrbits += 1
+
+        self.cloudClock = 0
+
+        self.taskFlag = 'Traveler-' + str(Orbiter.numOrbits)
+        taskMgr.add(self.Orbit, self.taskFlag)
+
+    def Orbit(self, task):
+        if self.orbitType == "MLB":
+            positionVec = defensePaths.BaseballSeams(task.time * Orbiter.velocity, self.numOrbits, 2.0)
+            self.modelNode.setPos(positionVec * self.orbitRadius + self.orbitObject.modelNode.getPos())
+
+        elif self.orbitType == "Cloud":
+            if self.cloudClock < Orbiter.cloudTimer:
+                self.cloudClock += 1
+
+            else:
+                self.cloudClock = 0
+                positionVec = defensePaths.Cloud()
+                self.modelNode.setPos(positionVec * self.orbitRadius + self.orbitObject.modelNode.getPos())
+
+        self.modelNode.lookAt(self.staringAt.modelNode)
+        return task.cont
+    
